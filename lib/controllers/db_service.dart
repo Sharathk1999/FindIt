b72ac7a60edc5777a3b6168cd6e64def28328678
Sharath Kumar <1999sharathkumar7@gmail.com>
2024-09-01 01:26:13 +0530

    Added firebase db func

--- lib/controllers/db_service.dart
+++ lib/controllers/db_service.dart
@@ -6,4 +6,28 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DbService {
   final db = FirebaseFirestore.instance;
+
+  //Categories
+
+//read category from db
+Stream<QuerySnapshot> readCategories(){
+  return db.collection("shop_categories")
+  .snapshots();
+}
+
+//create category
+Future createCategory({required Map<String, dynamic> data})async{
+  await db.collection("shop_categories").add(data);
+}
+
+//update category
+Future updateCategory({required String docId, required Map<String, dynamic> data})async{
+  await db.collection("shop_categories").doc(docId).update(data);
+}
+
+//delete category
+Future deleteCategory({required String docId})async{
+  await db.collection("shop_categories").doc(docId).delete();
+}
+
 }
